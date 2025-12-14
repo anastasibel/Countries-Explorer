b3dcae9e287fa347373e2980e036be2faa5fdc17
anastasibel <anabelova0221@gmail.com>
2025-12-14 20:04:00 +0400

    some fixes

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -55,13 +55,6 @@ dependencies {
     kapt(libs.google.hilt.compiler)
 
     api(libs.bundles.retrofit)
-    api(libs.github.glide.okhttp)
-
-//    val room_version = "2.6.1"
-//
-//    implementation("androidx.room:room-runtime:$room_version")
-//    implementation("androidx.room:room-ktx:$room_version")
-//    kapt("androidx.room:room-compiler:$room_version")
 
     kapt(libs.androidx.room.compiler)
     implementation(libs.androidx.room)
